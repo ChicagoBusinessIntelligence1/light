d708374ca5505192607cd86667f6fe2ed369fd17
Svetic <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-03-01 09:34:23 -0600

    Sidenav

--- app/scripts/auth/directives/sv-svet-auth.js
+++ app/scripts/auth/directives/sv-svet-auth.js
@@ -2,23 +2,17 @@
     'use strict';
 
     angular.module('auth')
-        .directive('svSvetAuth', function ($mdDialog,LxDialogService,LxNotificationService) {
+        .directive('svSvetAuth', function ($mdDialog, LxDialogService, LxNotificationService) {
             return {
                 templateUrl: 'scripts/auth/directives/sv-svet-auth.html',
                 restrict: 'E',
                 link: function ($scope, element, attr) {
-
-                    $scope.opendDialog = function(dialogId)
-                    {
+                    $scope.opendDialog = function (dialogId) {
                         LxDialogService.open(dialogId);
                     };
-
-                    $scope.closingDialog = function()
-                    {
+                    $scope.closingDialog = function () {
                         LxNotificationService.info('Dialog closed!');
                     };
-
-
                 }
             };
         });
