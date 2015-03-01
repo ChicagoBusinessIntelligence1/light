3237128f22b3523ba2880b8e508ac2f409c21f7c
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-01 10:06:24 -0600

    Login Button

--- app/scripts/auth/directives/sv-svet-auth.js
+++ app/scripts/auth/directives/sv-svet-auth.js
@@ -2,16 +2,16 @@
     'use strict';
 
     angular.module('auth')
-        .directive('svSvetAuth', function ($mdDialog, LxDialogService, LxNotificationService) {
+        .directive('svSvetAuth', function ($mdDialog, LxDialogService) {
             return {
                 templateUrl: 'scripts/auth/directives/sv-svet-auth.html',
                 restrict: 'E',
                 link: function ($scope, element, attr) {
-                    $scope.opendDialog = function (dialogId) {
-                        LxDialogService.open(dialogId);
+                    $scope.openLogin = function (loginId) {
+                        LxDialogService.open(loginId);
                     };
-                    $scope.closingDialog = function () {
-                        LxNotificationService.info('Dialog closed!');
+                    $scope.closeLogin = function (loginId) {
+                        LxDialogService.close(loginId);
                     };
                 }
             };
