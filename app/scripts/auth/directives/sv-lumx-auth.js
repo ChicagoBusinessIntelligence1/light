143682888a178ec10f650e70909c2c0d13b8001e
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-01 14:29:48 -0600

    Famous-Angular

--- app/scripts/auth/directives/sv-lumx-auth.js
+++ app/scripts/auth/directives/sv-lumx-auth.js
@@ -1,10 +1,24 @@
 (function () {
     'use strict';
     angular.module('auth')
-        .directive('svLumxAuth', function () {
+        .directive('svLumxAuth', function ($window) {
             return {
                 templateUrl: 'scripts/auth/directives/sv-lumx-auth.html',
                 link: function ($scope, element, attr) {
+                    $scope.w = angular.element($window);
+
+                    $scope.loginW = angular.element(element[0].children[0]).width();
+                    $scope.loginH = $window.innerHeight;
+
+
+                    $scope.w.bind('resize',function () {
+                        $scope.$apply(function () {
+
+                        $scope.loginW = angular.element(element[0].children[0]).width();
+                        $scope.loginH = $window.innerHeight;
+
+                        });
+                    });
 
                 }
             };
