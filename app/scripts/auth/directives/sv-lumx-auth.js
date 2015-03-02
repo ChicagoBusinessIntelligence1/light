89c65a405b38b807cb3604508c5790e6d5f25ba8
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-02 14:50:05 -0600

    Login Flippers

--- app/scripts/auth/directives/sv-lumx-auth.js
+++ app/scripts/auth/directives/sv-lumx-auth.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     angular.module('auth')
-        .directive('svLumxAuth', function ($window,$famous) {
+        .directive('svLumxAuth', function ($window, $famous) {
             function getDimensions(w) {
                 var large = 760;
                 var tablet = 500;
@@ -26,22 +26,12 @@
             return {
                 templateUrl: 'scripts/auth/directives/sv-lumx-auth.html',
                 controller: function ($scope) {
-                    $scope.flipIt = function() {
-                        $famous.find('fa-flipper')[0].flip();
+                    $scope.flipIt = function (flipperId) {
+                        $famous.find("#" + flipperId)[0].flip();
                     };
                 },
                 link: function ($scope, element, attr) {
                     $scope.win = angular.element($window);
-                    $scope.logWin = angular.element(element[0].children[0]);
-
-
-                    var w = angular.element(element[0].children[0]).width();
-                    var h = $scope.win.height();
-
-                    $scope.w = w;
-                    $scope.h = h;
-
-
                     $scope.win.bind('resize', function () {
                         $scope.$apply(function () {
 
@@ -53,6 +43,17 @@
 
                         });
                     });
+
+                    $scope.logWin = angular.element(element[0].children[0]);
+
+
+                    var w = angular.element(element[0].children[0]).width();
+                    var h = $scope.win.height();
+
+                    $scope.w = w;
+                    $scope.h = h;
+
+
 
 
                 }
