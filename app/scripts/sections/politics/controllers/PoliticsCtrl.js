f4e15c4da6efc697d8cc854c3d35548eba666fb9
Svetic <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-03-02 10:06:20 -0600

    Styles

--- app/scripts/sections/politics/controllers/PoliticsCtrl.js
+++ app/scripts/sections/politics/controllers/PoliticsCtrl.js
@@ -1,9 +1,24 @@
 (function () {
     'use strict';
-
     angular.module('sections.politics')
-        .controller('PoliticsCtrl', function ($scope) {
+        .controller('PoliticsCtrl', function ($scope, url, $firebase) {
+            var repo = url + 'posts';
+            $scope.posts = $firebase(new Firebase(repo)).$asArray();
 
+            $scope.addComment = function (postId, comment, userName, userPic, userLink) {
+                var commentsUrl = repo + '/' + postId + '/comments';
+                $scope.comments = $firebase(new Firebase(commentsUrl)).$asArray();
+                if (_.isUndefined(userLink)) {
+                    userLink = '#';
+                }
+                $scope.comments.$add(({
+                    comment: comment,
+                    userName: userName,
+                    userPic: userPic,
+                    userLink: userLink,
+                    time: (new Date).getTime()
+                }))
+            };
         });
 })();
 
