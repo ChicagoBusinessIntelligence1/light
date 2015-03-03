bc685c106497876af3728d987bbf81cff6c0653b
Svetic <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-03-03 11:56:44 -0600

    Grid List Design

--- app/scripts/common/directives/sv-content.js
+++ app/scripts/common/directives/sv-content.js
@@ -8,31 +8,12 @@
                 replace: true,
 
                 link: function ($scope, element, attr) {
-                    $scope.showGridBottomSheet = function ($event) {
-                        $scope.alert = '';
+                    $scope.openBottomSheet = function () {
                         $mdBottomSheet.show({
-                            templateUrl: 'scripts/footer/templates/social-media-links.html',
-                            controller: 'GridBottomSheetCtrl',
-                            targetEvent: $event
-                        }).then(function (clickedItem) {
-                            $scope.alert = clickedItem.name + ' clicked!';
+                            templateUrl: 'scripts/footer/templates/social-media-links.html'
                         });
                     };
                 }
             };
-        }).controller('GridBottomSheetCtrl', function ($scope, $mdBottomSheet) {
-            $scope.items = [
-                {name: 'Hangout', icon: 'hangout'},
-                {name: 'Mail', icon: 'mail'},
-                {name: 'Message', icon: 'message'},
-                {name: 'Copy', icon: 'copy'},
-                {name: 'Facebook', icon: 'facebook'},
-                {name: 'Twitter', icon: 'twitter'},
-            ];
-            $scope.listItemClick = function ($index) {
-                var clickedItem = $scope.items[$index];
-                $mdBottomSheet.hide(clickedItem);
-            };
         });
-
 })();
