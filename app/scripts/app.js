82bd9927125f9c297892474ab98f1296c7385cad
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-03 09:46:11 -0600

    Angular Aria console

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,68 +1,61 @@
 (function () {
 
-    'use strict';
+'use strict';
 
-    var app = angular.module('app', [
+var app = angular.module('app', [
 
-        // Common modules
-        'header',
-        'aside',
-        'common',
-        'auth',
-        'contactus',
-        'footer',
+    // Common modules
+    'header',
+    'aside',
+    'common',
+    'auth',
+    'contactus',
+    'footer',
 
-        // Site section modules
-        'sections.home',
-        'sections.politics',
+    // Site section modules
+    'sections.home',
+    'sections.politics',
 
-        // 3rd party modules
-        'firebase',
-        'ngAnimate',
-        'ngMaterial',
-        'ui.router'
-    ])
-        .config(function ($mdThemingProvider) {
-            $mdThemingProvider.theme('default')
-                .primaryPalette('indigo')
-                .backgroundPalette('grey')
-                .accentPalette('grey');
-        })
-        .config(function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise('/home');
+    // 3rd party modules
+    'firebase',
+    'ngAnimate',
+    'ngMaterial',
+    'ui.router'
+])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
 
+        $stateProvider
+            .state('app', {
+                abstract: true,
+                templateUrl: 'scripts/sections/home/views/main.html'
 
-            $stateProvider
-                .state('app', {
-                    abstract: true,
-                    templateUrl: 'scripts/sections/home/views/main.html'
+            })
+            .state('app.home', {
+                url: '/home',
+                controller: 'Home',
+                templateUrl: 'scripts/sections/home/views/home.html'
+            })
 
-                })
-                .state('app.home', {
-                    url: '/home',
-                    controller: 'Home',
-                    templateUrl: 'scripts/sections/home/views/home.html'
-                })
+            .state('app.contact', {
+                url: '/contact',
+                controller: 'ContactusCtrl',
+                templateUrl: '../views/contactus.html'
+            })
+            .state('app.admin', {
+                url: '/admin',
+                controller: 'LoginCtrl',
+                templateUrl: '../views/login.html'
+            })
 
-                .state('app.contact', {
-                    url: '/contact',
-                    controller: 'ContactusCtrl',
-                    templateUrl: '../views/contactus.html'
-                })
-                .state('app.admin', {
-                    url: '/admin',
-                    controller: 'LoginCtrl',
-                    templateUrl: '../views/login.html'
-                })
-
-                .state("app.politics", {
-                    url: "/politics",
-                    controller: "PoliticsCtrl",
-                    templateUrl: "scripts/sections/politics/views/politics.html"
-                })
+			.state("app.politics", {
+				url: "/politics",
+				controller:"PoliticsCtrl",
+				templateUrl: "scripts/sections/politics/views/politics.html"
+			})
 //#state
-        });
+    });
 
-    app.value('url', 'https://svet.firebaseio.com/');
+app.value('url', 'https://svet.firebaseio.com/');
 
 })();
