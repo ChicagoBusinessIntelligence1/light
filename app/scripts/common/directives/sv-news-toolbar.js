3d47cdbe66d599371508a91a6206726b9331742d
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-07 18:52:08 -0600

    Service Generator

--- app/scripts/common/directives/sv-news-toolbar.js
+++ app/scripts/common/directives/sv-news-toolbar.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('common')
-        .directive('svNewsToolbar', function (sdf) {
+        .directive('svNewsToolbar', function () {
             return {
                 templateUrl: 'scripts/common/directives/sv-news-toolbar.html',
                 scope:{},
