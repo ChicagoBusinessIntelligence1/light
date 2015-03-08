4e676a44daa9e555dddfc32a55a09d714eb83682
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-08 12:57:55 -0500

    Directives

--- app/scripts/common/directives/sv-home-second-news.js
+++ app/scripts/common/directives/sv-home-second-news.js
@@ -7,7 +7,11 @@
                 templateUrl: 'scripts/common/directives/sv-home-second-news.html',
                 scope:{},
                 link: function ($scope, element, attr) {
+                    $scope.secondTiles = [];
 
+                    for (var i = 0; i < 10; i++) {
+                        $scope.secondTiles.push(i);
+                    }
                 }
             };
         });
