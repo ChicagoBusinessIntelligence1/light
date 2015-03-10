435bf428bc0710866d0ce679b6672296048a7497
Svetic <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-03-10 12:51:20 -0500

    test

--- app/scripts/common/directives/sv-main-left.js
+++ app/scripts/common/directives/sv-main-left.js
@@ -4,7 +4,6 @@
     angular.module('common')
         .directive('svMainLeft', function () {
             return {
-                replace: true,
                 templateUrl: 'scripts/common/directives/sv-main-left.html',
                 link: function ($scope, element, attr) {
 
