f1ff8b735ef4825431e1def031cf1b06ce3956a4
Svetic <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-03-11 12:43:46 -0500

    Gas Widget

--- app/scripts/common/news/directives/sv-news-toolbar.js
+++ app/scripts/common/news/directives/sv-news-toolbar.js
@@ -2,19 +2,17 @@
     'use strict';
 
     angular.module('common')
-        .directive('svNewsToolbar', function (NewsGeneratorServ,$rootScope) {
+        .directive('svNewsToolbar', function (NewsGeneratorServ, $rootScope) {
             return {
-                templateUrl: 'scripts/common/directives/sv-news-toolbar.html',
+                templateUrl: 'scripts/common/news/directives/sv-news-toolbar.html',
                 scope: {
                     number: '=',
                     panelTitle: '@'
                 },
                 link: function ($scope, element, attr) {
                     $rootScope.$watch('allNews', function (newValue, oldValue) {
-                        $scope.news = _.take(_.shuffle(_.rest(newValue,3)), $scope.number);
-
+                        $scope.news = _.take(_.shuffle(_.rest(newValue, 3)), $scope.number);
                     });
-
                 }
             };
         });
