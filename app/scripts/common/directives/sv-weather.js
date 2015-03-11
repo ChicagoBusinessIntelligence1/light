601292a7f680638db7331402ba42b0458da08018
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-11 11:36:58 -0500

    Add files

---
+++ app/scripts/common/directives/sv-weather.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    angular.module('common')
+        .directive('svWeather', function () {
+            return {
+                templateUrl: 'scripts/common/directives/sv-weather.html',
+                scope:{},
+                link: function ($scope, element, attr) {
+
+                }
+            };
+        });
+})();
