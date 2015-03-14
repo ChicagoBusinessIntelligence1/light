094c404579690c177d7eeda77cc64a2d47454465
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-14 14:47:47 -0500

    Weather widget. Correct Icons

--- app/scripts/widgets/filters/IconProcess.js
+++ app/scripts/widgets/filters/IconProcess.js
@@ -3,8 +3,26 @@
 
     angular.module('widgets')
         .filter('IconProcess', function () {
-            return function (input) {
-                return 'test filter: ' + input;
+            return function (icon) {
+                var finalIcon;
+                switch (true) {
+                    case icon.indexOf('clear')>-1:
+                        finalIcon = 'weather-sunny'
+                        break;
+                    case icon.indexOf('cloudy')>-1:
+                        finalIcon = 'partly-cloudy'
+                        break;
+                    case icon.indexOf('rain')>-1:
+                        finalIcon = 'rain'
+                        break;
+                    case icon.indexOf('snow')>-1:
+                        finalIcon = 'snow'
+                        break;
+                    case icon.indexOf('mist')>-1:
+                        finalIcon = 'mist'
+                        break;
+                }
+                return finalIcon;
             };
         });
 })();
