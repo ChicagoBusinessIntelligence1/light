9560b9a49ada052d2d2daef6c841ee91d4114c16
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-14 14:55:22 -0500

    Fore Cast for tomorrow/day after tomorrow

--- app/scripts/widgets/directives/sv-weather.js
+++ app/scripts/widgets/directives/sv-weather.js
@@ -3,27 +3,6 @@
 
     angular.module('widgets')
         .directive('svWeather', function (WeatherServ) {
-            function processIcon (icon) {
-                var finalIcon;
-                switch (true) {
-                    case icon.indexOf('clear')>-1:
-                       finalIcon = 'weather-sunny'
-                        break;
-                    case icon.indexOf('cloudy')>-1:
-                       finalIcon = 'partly-cloudy'
-                        break;
-                    case icon.indexOf('rain')>-1:
-                        finalIcon = 'rain'
-                        break;
-                    case icon.indexOf('snow')>-1:
-                        finalIcon = 'snow'
-                        break;
-                    case icon.indexOf('mist')>-1:
-                        finalIcon = 'mist'
-                        break;
-                }
-                return finalIcon;
-            }
 
             return {
                 templateUrl: 'scripts/widgets/directives/sv-weather.html',
@@ -32,9 +11,6 @@
                     var weatherObj = WeatherServ.getWeatherObj();
                     $scope.weather = weatherObj;
 
-                    weatherObj.$loaded().then(function (w) {
-                        $scope.icon = processIcon(w.currently.icon);
-                    })
 
                     weatherObj.$bindTo($scope, 'weather')
                 }
