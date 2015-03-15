2edddb014995ba63dd10b0fc9301d99231ef7f51
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-15 16:13:43 -0500

    Everything Works

--- app/scripts/common/controllers/ArticleCtrl.js
+++ app/scripts/common/controllers/ArticleCtrl.js
@@ -2,7 +2,10 @@
     'use strict';
 
     angular.module('common')
-        .controller('ArticleCtrl', function ($scope,$location,NewsCategoriesServ) {
+        .controller('ArticleCtrl', function ($scope,$stateParams,$rootScope,$location,NewsCategoriesServ) {
+            var id = $stateParams.id;
+            $scope.tag = $rootScope.allCateg[id];
+
             $scope.news =  NewsCategoriesServ.getCategoryNews($location);
 
         });
