64da4c51d5a5acb0082ef6883217af08cd4a86d3
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-03-16 11:06:45 -0500

    Add files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -153,16 +153,6 @@
                     controller: "ClassifiedCtrl",
                     templateUrl: "scripts/sections/classified/views/classifiedCtrl.html"
                 })
-                //.state("app.politics", {
-                //    url: "/politics",
-                //    controller: "Politics",
-                //    templateUrl: "scripts/sections/politics/views/politicsCtrl.html"
-                //})
-                //
-                //.state("app.politics.article", {
-                //    url: "/:id",
-                //    templateUrl: "scripts/sections/politics/views/politics-article.html"
-                //})
                 .state("app.news", {
                     url: "/news/:id",
                     controller: "SvetNewsCtrl",
