65e3c66196baeaef0aeef54b96426f9169d07499
Svetic <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-03-16 15:27:14 -0500

    Add files

--- app/scripts/common/directives/sv-pdf-issue.js
+++ app/scripts/common/directives/sv-pdf-issue.js
@@ -7,28 +7,6 @@
                 templateUrl: 'scripts/common/directives/sv-pdf-issue.html',
                 scope:{},
                 link: function ($scope, element, attr) {
-                var flip = element.find('.flipbook');
-                    flip.turn({
-
-                        width:922,
-
-                        // Height
-
-                        height:600,
-
-                        // Elevation
-
-                        elevation: 50,
-
-                        // Enable gradients
-
-                        gradients: true,
-
-                        // Auto center this flipbook
-
-                        autoCenter: true
-
-                    });
                 }
             };
         });
