c39c18524ae49dfbe9c67e5808f410220e3a69f0
Svetic <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-03-18 11:38:24 -0500

    Add files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -168,11 +168,11 @@
                     controller: "PdfArchiveCtrl",
                     templateUrl: "scripts/common/views/pdf-archiveCtrl.html"
                 })
-                .state("app.create-article", {
-                    url: "/create-article",
-                    controller: "Create-articleCtrl",
-                    templateUrl: "scripts/common/article/views/create-articleCtrl.html"
-                })
+			.state("app.create-article", {
+				url: "/create-article", 
+				controller:"CreateArticleCtrl",
+				templateUrl: "scripts/common/article/views/create-articleCtrl.html"
+			})
 //#state
         });
 
