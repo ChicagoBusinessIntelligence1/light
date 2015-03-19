ba3a84d374703c204fd974ce164fa20e5e87579a
Svetic <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-03-19 11:09:17 -0500

    Datepicker Widget Bug Fixed

--- app/scripts/common/directives/sv-main-content.js
+++ app/scripts/common/directives/sv-main-content.js
@@ -4,6 +4,7 @@
     angular.module('common')
         .directive('svMainContent', function () {
             return {
+                replace: true,
                 templateUrl: 'scripts/common/directives/sv-main-content.html',
                 link: function ($scope, element, attr) {
 
