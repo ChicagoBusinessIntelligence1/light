8827dc252ac97c4080915bc47916c1fdd43acdb0
Svetic <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-03-19 18:20:04 -0500

    Module <!-- common.article files in index.html

--- app/scripts/common/article/directives/sv-social-share.js
+++ app/scripts/common/article/directives/sv-social-share.js
@@ -4,7 +4,7 @@
     angular.module('common.article')
         .directive('svSocialShare', function () {
             return {
-                replace:true,
+                replace: true,
                 templateUrl: 'scripts/common/article/directives/sv-social-share.html',
                 link: function ($scope, element, attr) {
                 }
