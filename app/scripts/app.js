3002dd582c5d186354c6a609abf229ce3a0f3c61
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-03-23 13:17:22 -0500

    Add files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -151,6 +151,11 @@
                     templateUrl: "scripts/auth/views/svet-loginCtrl.html"
                 })
 			
+			.state("app.ramala", {
+				url: "/ramala", 
+				controller:"RamalaCtrl as Ramala",
+				templateUrl: "scripts/auth/views/ramalaCtrl.html"
+			})
 //#state
         });
 
---
+++ app/scripts/auth/controllers/RamalaCtrl.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    angular.module('auth')
+        .controller('RamalaCtrl', function ($scope) {
+            var ramala = this;
+        });
+})();
+
