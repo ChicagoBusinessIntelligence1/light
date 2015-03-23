d5239ff9a9c7c913ecf8b53c107e415ccbcc9804
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-03-23 15:22:43 -0500

    Generating Ctrl with ctrl as. Directives with dir

--- app/bower_components/angular/angular.js
+++ app/bower_components/angular/angular.js
@@ -9983,8 +9983,6 @@ function createHttpBackend($browser, createXhr, $browserDefer, callbacks, rawDoc
       };
 
       var requestError = function() {
-        // The response is always empty
-        // See https://xhr.spec.whatwg.org/#request-error-steps and https://fetch.spec.whatwg.org/#concept-network-error
         completeRequest(callback, -1, null, null, '');
       };
 
