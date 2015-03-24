a7c28bfd27169bd642ea645b7c89b67d54dff538
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-03-24 12:37:12 -0500

    Header Z-index

--- app/scripts/auth/directives/sv-auth-btn.js
+++ app/scripts/auth/directives/sv-auth-btn.js
@@ -6,16 +6,8 @@
                 templateUrl: 'scripts/auth/directives/sv-auth-btn.html',
                 restrict: 'E',
                 link: function ($scope, element, attr) {
-                    $scope.showAdvanced = function (ev) {
-                        $mdDialog.show({
-                            templateUrl: 'scripts/auth/directives/sv-auth-dialog.html',
-                            targetEvent: ev
-                        }).then(function (answer) {
-                            $scope.alert = 'You said the information was "' + answer + '".';
-                        }, function () {
-                            $scope.alert = 'You cancelled the dialog.';
-                        });
-                    };
+
+
 
 
 
