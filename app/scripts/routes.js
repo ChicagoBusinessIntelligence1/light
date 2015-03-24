f2e1bfa87101cc6aa4dc33fc76a1bddb788cb8ce
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-03-24 10:15:07 -0500

    Add files

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -105,11 +105,12 @@
                     controller: "SvetLoginCtrl as svetLogin",
                     templateUrl: "scripts/auth/views/svet-loginCtrl.html"
                 })
-            			.state("app.-controller", {
-				url: "/-controller", 
-				controller:"ControllerCtrl as controller",
-				templateUrl: "scripts/auth/views/-controllerCtrl.html"
-			})
+
+                .state("app.-controller", {
+                    url: "/-controller",
+                    controller: "ControllerCtrl as controller",
+                    templateUrl: "scripts/auth/views/-controllerCtrl.html"
+                })
 //#state'
 
         });
